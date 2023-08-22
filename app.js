56d732d7edb6892b627d58222f2206ce9cf1b142
Sanek182 <maxpower_90>
2023-08-22 20:13:22 +0300

    ensuring authentification logic from the server side

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-//var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const port = process.env.PORT || 3500;
 const cors = require('cors');
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
